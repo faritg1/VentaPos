80969bcbc034c318726c4a788cb7d392aef9021c
GH0STDARK <nietomaicol45@gmail.com>
2025-09-17 18:07:14 -0500

    menu mejorado

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('title', '🖥️ Panel de Administración')
+
+@section('content_header')
+    <h1>🖥️ Panel de Administración</h1>
+@stop
+
+@section('content')
+<div class="container-fluid">
+    <!-- Aquí va todo el contenido de admin -->
+</div>
+@stop
